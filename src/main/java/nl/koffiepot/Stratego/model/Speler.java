a2e36b14831f2f5dcf1637f765875f0c2cb18038
FernonE <fernon94@hotmail.com>
2019-05-10 14:00:21 +0200

    Added database functionality, able to save player, nothing more

--- src/main/java/nl/koffiepot/Stratego/model/Speler.java
+++ src/main/java/nl/koffiepot/Stratego/model/Speler.java
@@ -3,16 +3,15 @@ package nl.koffiepot.Stratego.model;
 import java.util.Scanner;
 
 public class Speler {
-    private long id;
-    private String spelerNaam;
-    private int spelerWins;
-    private int spelerLosses;
     private int spelerTeam;
     private boolean gewonnen;
+
+
+    //Variabelen nodig voor de functies
     private Scanner scanner = new Scanner(System.in);
     private int[] selectCoords = {0,0}; //deze wordt aangepast
 
-
+//
     private int[] selectPiece(){
         /*
         -vragen om user input
@@ -51,7 +50,8 @@ public class Speler {
                 passed = false;
                 continue; //als het misgaat, springt java vanaf hier meteen naar de while(!passed) en slaat de volgende check dus over. Aangezien dat niet gaat :)
             }
-            if(!bord.checkValidPiece(selectCoords[1],selectCoords[0],this.spelerTeam)){ //daarna kijken of het wel een correcte speelstuk is
+//LET OP VOLGENDE STUK CODE HEB IK AANGEPAST ZODAT HET EVEN WERKT VOOR DATABASE!!!
+            if(!bord.checkValidPiece(selectCoords[1],selectCoords[0],1)){//this.spelerTeam)){ Dit heb ik kort aangepast om database werkende te krijgen //daarna kijken of het wel een correcte speelstuk is
                 //prints wanneer iets verkeerd gekozen is gebeurt al in bord.
                 passed = false;
             }
@@ -70,45 +70,6 @@ public class Speler {
         } while (!passed);
     }
 
-
-    public Speler(String spelerNaam,int spelerTeam) {
-        this.spelerNaam = spelerNaam;
-        this.spelerTeam = spelerTeam;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getSpeler() {
-        return spelerNaam;
-    }
-
-    public void setSpeler(String speler) {
-        this.spelerNaam = speler;
-    }
-
-    public int getSpelerWins() {
-        return spelerWins;
-    }
-
-
-    public void setSpelerWins(int spelerWins) {
-        this.spelerWins = spelerWins;
-    }
-
-    public int getSpelerLosses() {
-        return spelerLosses;
-    }
-
-    public void setSpelerLosses(int spelerLosses) {
-        this.spelerLosses = spelerLosses;
-    }
-
     public int getSpelerTeam() {
         return spelerTeam;
     }
@@ -123,13 +84,5 @@ public class Speler {
 
     public void setGewonnen(boolean gewonnen) {
         this.gewonnen = gewonnen;
-    }
-
-    @Override
-    public String toString() {
-        return "Speler{" +
-                "Naam='" + spelerNaam + '\'' +
-                ", Team=" + (spelerTeam==0 ? "Red":"Blue") +
-                '}';
     }
 }
